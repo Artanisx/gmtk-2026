eb8b4cd0e28a30a073dfdb2ce0c968690bab99b1
Artanis <fabrizio+git@fabriziotobia.it>
2026-07-23 11:03:38 +0200

    Initial work
    Created a folder structure, a test scene and the Character superclass.

---
+++ Game Jam/Assets/Code/Scripts/NPC/Character.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+// Basic Class for all Characters in the game (Player, NPCs)
+// It includes basic states and variables
+
+public enum CharacterType
+{
+    PLAYER,
+    NPCSTAFF,
+    NPCGUARD
+}
+
+public enum CharacterState
+{
+    IDLE,           // IDLE STATE, USEFUL FOR PLAY IDLE ANIMATION
+    WALK,           // WALK STATE, USED FOR THE PLAYER AND RELATED ANIMATION
+    HACK,           // HACK STATE, USED FOR THE PLAYER TO HACK A MACHINE
+    PATROL,         // PATROL STATE, USED FOR THE NPC GUARD TO MOVE AROUND WAYPOINTS 
+    CHASE,          // CHASE STATE, USED FOR THE NPC GUARD TO CHASE THE PLAYER AROUND THE MAP
+    CHECK,          // CHECK STATE, USED FOR THE NPC GUARD TO CHECK THE MACHINE FOR TAMPERING
+    ARREST,         // ARREST STATE, USED FOR THE NPC GUARD TO ARREST THE PLAYER ONCE THEY CAUGHT THEM
+    WORK            // WORK STATE, USED FOR THE NPC STAFF TO MAN THE MACHINE (DEALING CARDS etc)
+}
+ 
+
+public class Character
+{
+    public CharacterType Type;
+    public CharacterState State = CharacterState.IDLE;
+    
+    public float WalkSpeed;
+    public float RunSpeed;
+    
+    public Character(CharacterType characterType)
+    {
+        Type = characterType;
+    }
+}
