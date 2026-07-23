9a5898c10eaf864f880db2a09d4183eae22018f1
cruise4u <pedrofrancisco90@gmail.com>
2026-07-23 16:08:54 +0100

    Updated logic for the casino machine states and interaction

--- Game Jam/Assets/Scripts/Machine/CasinoMachine.cs
+++ Game Jam/Assets/Scripts/Machine/CasinoMachine.cs
@@ -2,57 +2,88 @@ using UnityEngine;
 
 public class CasinoMachine : MonoBehaviour
 {
-    // boolean to decide if it's hacked or not.
-    public bool isMachineHacked;
-
-    // basic enum for states since there's only 2 and doesn't need to have more functionality like player, guards,etc
+    // Testing material
+    public Material stateMaterial;
+    
+    //Enum of machine states
     public enum ECasinoMachineState
     {
         Working,
         Broken,
     }
+
+    // boolean to flag if it's hacked or not.
+    public bool isMachineHacked;
     
-    private ECasinoMachineState myState;
+    // basic enum for states since there's only 2 and doesn't need to have more functionality like player, guards,etc
+    [SerializeField] ECasinoMachineState myState;
     
     // Maybe we can use Events for simplify the communication between entities? Not doing it for now..
     // But the machine decides it's own state instead of being done in the PlayerSabotage script..
-    // Will also help with when something happens -> sends message to other systems (AI,UI,etc)
-    public void GetHacked()
-    {
-        isMachineHacked = true;
-        SetMachineState(ECasinoMachineState.Working);
-        // Perform some more operations here 
-    }
-
-    public void GetFixed()
-    {
-        isMachineHacked = false;
-        SetMachineState(ECasinoMachineState.Broken);
-    }
     
-    public void SetMachineState(ECasinoMachineState state)
+    //Depending on it's state either get's sabotaged or fixed
+    public void GetSabotaged()
     {
-        if (state == ECasinoMachineState.Working)
+        if (myState == ECasinoMachineState.Working)
         {
-            // 
+            GetBroken();
         }
         else
         {
+            GetFixed();   
+        }
+    }
+
+    //Get broken will make it start to countdown and get money from the casino
+    //KASHING!
+    public void GetBroken()
+    {
+        isMachineHacked = true;
+        SetMachineState(ECasinoMachineState.Broken);
+
+        Debug.Log("boolean value is : " + isMachineHacked);
+        Debug.Log("My state is: " + myState);
+    }
+
+    //Get Fixed will make it stop counting down from this pool of money
+    //ohhhh!
+    public void GetFixed()
+    {
+        isMachineHacked = false;
+        SetMachineState(ECasinoMachineState.Working);
+        
+        Debug.Log("boolean value is : " + isMachineHacked);
+        Debug.Log("My state is: " + myState);
+
+    }
+    
+    // Not even a state machine, just an enum for having 2 different states
+    // A bit hardcoded, but sets itself the state based on the parameter state.. quite obvious
+    // it can also do some internal stuff (UI,VFX,SOUND,etc) later on..
+    public void SetMachineState(ECasinoMachineState state)
+    {
+        if (state == ECasinoMachineState.Working)
+        {   
+            myState = ECasinoMachineState.Working;
+            GetComponent<MeshRenderer>().material = stateMaterial;
+            stateMaterial.color = Color.blue;
+            // Does something here? 
+            // Update UI, Art?
+        }
+        else
+        {
+            myState = ECasinoMachineState.Broken;
+            GetComponent<MeshRenderer>().material = stateMaterial;
+            stateMaterial.color = Color.darkRed;
             // Probably signal "Guard/AI" to come check it? 
         }
     }
-    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         isMachineHacked = false;
-        myState =  ECasinoMachineState.Working;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SetMachineState(ECasinoMachineState.Working);
+        Debug.Log("My state is: " + myState);
     }
 }
