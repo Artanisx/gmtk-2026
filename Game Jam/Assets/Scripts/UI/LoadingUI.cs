40c74b2b000fee1c8e6d98677a38dd2e683591b3
ModAndThink <druidemante@gmail.com>
2026-07-24 16:58:54 +0200

    Finish loading screen

--- Game Jam/Assets/Scripts/UI/LoadingUI.cs
+++ Game Jam/Assets/Scripts/UI/LoadingUI.cs
@@ -1,34 +1,44 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingUI : MonoBehaviour
 {
     [SerializeField]
     private Animator animator;
+    [SerializeField]
+    private int sceneId;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
 
-        StartCoroutine(TriggerLoading(1));
+        animator.Play("SlideOut");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Change scene
+    public void TriggerLoading() => StartCoroutine(TriggerLoading(sceneId));
 
-    public IEnumerator TriggerLoading(int sceneId)
+    private IEnumerator TriggerLoading(int sceneId)
     {
         yield return new WaitForSeconds(1);
 
         animator.Play("SlideIn");
 
-        while (animator.GetAnimatorTransitionInfo(0).normalizedTime > 1.0f) yield return null;
+        // load scene in the background
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneId);
+        asyncLoad.allowSceneActivation = false;
 
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(1);
 
-        animator.Play("SlideOut");
+        // while the scene load we update the progress bar
+        while (!asyncLoad.isDone)
+        {
+            // check if it has finished
+
+            if (asyncLoad.progress >= 0.9f) asyncLoad.allowSceneActivation = true;
+
+            yield return null;
+        }
     }
 }
