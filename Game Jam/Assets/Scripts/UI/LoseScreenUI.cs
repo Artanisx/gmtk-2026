b2426abaeb4f55f423dcf966977ac5ff05cd35f6
ModAndThink <druidemante@gmail.com>
2026-07-24 19:41:52 +0200

    Adding a losing menu with sound and all

---
+++ Game Jam/Assets/Scripts/UI/LoseScreenUI.cs
@@ -0,0 +1,35 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LoseScreenUI : MonoBehaviour
+{
+    [SerializeField] private LoadingUI menuLoadingScreen;
+    [SerializeField] private LoadingUI levelLoadingScreen;
+    private Animator animator;
+    private AudioSource losingSound;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // get component
+        animator = gameObject.GetComponent<Animator>();
+        losingSound = gameObject.GetComponent<AudioSource>();
+
+        OnLoss();
+    }
+
+    public void OnLoss()
+    {
+        animator.Play("LoseFlash");
+        losingSound.Play();
+    }
+
+    public void OnRetry()
+    {
+        levelLoadingScreen.TriggerLoading();
+    }
+
+    public void OnMenu()
+    {
+        menuLoadingScreen.TriggerLoading();
+    }
+}
