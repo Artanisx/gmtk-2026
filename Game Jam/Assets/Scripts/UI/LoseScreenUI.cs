be2d512af0dec28001ad02cbd5167871d79dd933
Artanis <fabrizio+git@fabriziotobia.it>
2026-07-25 15:36:16 +0200

    If the player is caught while hacking, the hacking minigame is destroyed to properly show the loss ui

--- Game Jam/Assets/Scripts/UI/LoseScreenUI.cs
+++ Game Jam/Assets/Scripts/UI/LoseScreenUI.cs
@@ -7,6 +7,13 @@ public class LoseScreenUI : MonoBehaviour
     [SerializeField] private LoadingUI levelLoadingScreen;
     private Animator animator;
     private AudioSource losingSound;
+    private GameSystem gameSystem;
+
+    void Awake()
+    {
+        gameSystem = GameObject.Find("GameSystem").GetComponent<GameSystem>();
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,6 +29,15 @@ public class LoseScreenUI : MonoBehaviour
 
     public void OnLoss()
     {
+        // Check if the minigame was running, if so destroy it
+        if (gameSystem.IsMinigameSpawned)
+        {
+            HackMinigameInput hackingMinigame = GameObject.Find("Hacking Minigame(Clone)").GetComponent<HackMinigameInput>();
+            if (hackingMinigame == null)
+                Debug.LogError("I couldn't find the minigame object even though it is running.");
+            hackingMinigame.DestroyMinigame();
+        }
+        
         gameObject.SetActive(true);
         animator.Play("LoseFlash");
         losingSound.Play();
