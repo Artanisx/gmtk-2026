31343ae3626bf308ca5585081cc73b45a0a1f498
Artanis <fabrizio+git@fabriziotobia.it>
2026-07-25 15:42:12 +0200

    The miningame should also be destroyed if the player wins.

--- Game Jam/Assets/Scripts/UI/WinScreenUI.cs
+++ Game Jam/Assets/Scripts/UI/WinScreenUI.cs
@@ -5,6 +5,7 @@ public class WinScreenUI : MonoBehaviour
     [SerializeField] private LoadingUI menuLoadingScreen;
     private Animator _animator;
     private AudioSource _winSound;
+    private GameSystem gameSystem;
     
     private void Awake()
     {
@@ -16,10 +17,21 @@ public class WinScreenUI : MonoBehaviour
 
         // hide win screen
         gameObject.SetActive(false);
+        
+        gameSystem = GameObject.Find("GameSystem").GetComponent<GameSystem>();
     }
 
     private void OnWin()
     {
+        // Check if the minigame was running, if so destroy it
+        if (gameSystem.IsMinigameSpawned)
+        {
+            HackMinigameInput hackingMinigame = GameObject.Find("Hacking Minigame(Clone)").GetComponent<HackMinigameInput>();
+            if (hackingMinigame == null)
+                Debug.LogError("I couldn't find the minigame object even though it is running.");
+            hackingMinigame.DestroyMinigame();
+        }
+        
         gameObject.SetActive(true);
     }
     
