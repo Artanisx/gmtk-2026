438b78909427d2b2fb6eae66713246cebd7efe0a
Artanis <fabrizio+git@fabriziotobia.it>
2026-07-26 10:39:47 +0200

    Reworked the LoadingUI to accomodate the HelpScreen
    Now the LoadingUI is able to load multiple scenes.
    Added a Istructiosn button and reworked the PlayButton to have a variable that selects the scene to load.
    Clicking on PLAY will load the scene in the playbuttonscript variable (3 for now);
    Clicking on ISTRUCTIONS will load the scene in the instructionsbuttonscript variable (4 for now);
    
    Added also a stub of the help screen.

--- Game Jam/Assets/Scripts/UI/PlayButtonScript.cs
+++ Game Jam/Assets/Scripts/UI/PlayButtonScript.cs
@@ -7,8 +7,13 @@ using UnityEngine.SceneManagement;
 public class PlayButtonScript : MonoBehaviour
 {
     public LoadingUI loadingUI;
+    
+    [Tooltip("The scene id of the Play Scene. This will be loaded when the player clicks this button.")]
+    public int PlaySceneID = 3;
+    
     public void OnPlay()
     {
+        loadingUI.SetSceneID(PlaySceneID);
         loadingUI.TriggerLoading();
     }
 }
